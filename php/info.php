53fb382d1e7747d418954ded46cc84e60011da8d
Shashank Tiwari <106220488+shashankktiwariii@users.noreply.github.com>
2022-09-10 08:50:34 +0530

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,8 +1,5 @@
 <?php 
 $base_url = "//{$_SERVER['SERVER_NAME']}";
 $website_name = "GogoAnime";
-$apiLink = "https://anikatsu2.herokuapp.com"; 
-if (date("d") > 15){
-    $apiLink = "https://anikatsu.herokuapp.com";
-}
+$apiLink = "https://verel-api.vercel.app/api"; 
 ?>
